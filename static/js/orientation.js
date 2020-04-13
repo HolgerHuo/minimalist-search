37df0a0628e5d8a7937f1ffde7e24789bd1a4a19
HolgerHuo <holgerhuo@outlook.com>
2020-04-13 22:08:52 +0800

    Prepare mobile icon only mode

---
+++ static/js/orientation.js
@@ -0,0 +1,13 @@
+var mql = window.matchMedia('(orientation: portrait)');
+console.log(mql);
+function onMatchMediaChange(mql){
+    if(mql.matches){
+        $("txtq").css("background-color","none"); 
+        console.log('Portrait enabled.')
+    }else{
+        $("txtq").removeClass("quick_hid"); 
+        console.log('Landscape activated.')
+    }
+}
+onMatchMediaChange(mql);
+mql.addListener(onMatchMediaChange);
