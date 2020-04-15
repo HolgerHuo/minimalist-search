bda6c09002da520dcd5213fb88b4928af50656e3
HolgerHuo <holgerhuo@outlook.com>
2020-04-15 14:29:16 +0800

    New color shift engine.

---
+++ static/js/theme_auto.js
@@ -0,0 +1,59 @@
+const hasNativePrefersColorScheme =
+        window.matchMedia('(prefers-color-scheme)').media !== 'not all';
+let theme_value = localStorage.getItem('theme'),
+    daily_theme_list = [], dark_theme_list = [];
+
+daily_theme_list.push(document.getElementById('daily-theme'));
+dark_theme_list.push(document.getElementById('dark-theme'));
+let loadMode = function(mode) {
+    if (mode !== "daily" && mode !== "dark") return;
+    daily_theme_list.forEach((obj) => {
+        if (mode === "daily") {
+            if (obj.hasAttribute('disabled')) {
+                obj.removeAttribute('disabled');
+            }
+            obj.setAttribute('media', 'all');
+        } else {
+            obj.setAttribute('disabled', '');
+            obj.setAttribute('media', obj.getAttribute('data-original-media'));
+        }
+    });
+    dark_theme_list.forEach((obj) => {
+        if (mode === "daily") {
+            obj.setAttribute('disabled', '');
+            obj.setAttribute('media', obj.getAttribute('data-original-media'));
+        } else {
+            if (obj.hasAttribute('disabled')) {
+                obj.removeAttribute('disabled');
+            }
+            obj.setAttribute('media', 'all');
+        }
+    });
+    localStorage.setItem('theme', mode);
+    theme_value = mode;
+};
+window.matchMedia('(prefers-color-scheme: dark)').addListener(({matches}) => {
+    dispatchEvent(new CustomEvent("colorschemechange", {
+        detail: {colorScheme: (matches ? "dark" : "daily")}
+    }));
+});
+if(Cookies.get('theme')!=="1"&&Cookies.get('theme')!=="0"){
+    if ((window.matchMedia('(prefers-color-scheme: light)').matches) ||
+        (window.matchMedia('(prefers-color-scheme: no-preference)').matches)) {
+        $("#theme-switch-icon").attr("src", "static/icon/icon-light.svg");
+        Cookies.set('theme',2);
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        $("#theme-switch-icon").attr("src", "static/icon/icon-dark.svg");
+        Cookies.set('theme',3);
+    }
+    window.addEventListener('colorschemechange', (e) => {
+        if(e.detail.colorScheme == "dark"){
+            $("#theme-switch-icon").attr("src", "static/icon/icon-dark.svg")
+            Cookies.set('theme',3);
+        }
+        if(e.detail.colorScheme == "daily"){
+            $("#theme-switch-icon").attr("src", "static/icon/icon-light.svg")
+            Cookies.set('theme',2);
+        }
+    });
+}
---
+++ static/js/theme_manual.js
@@ -0,0 +1,39 @@
+/* manual switch*/
+function themeDark(){$('head').append('<link rel="stylesheet" href="static/css/sou_night.css" />');}
+function themeLight(){$('head').append('<link rel="stylesheet" href="static/css/sou.css" />');}
+function initThemeSwitch(){
+    theme = Cookies.get('theme');
+	if(theme == '1'){
+		$("#theme-switch-icon").attr("src", "static/icon/icon-dark.svg");
+    }
+    if(theme == '0'){
+		$("#theme-switch-icon").attr("src", "static/icon/icon-light.svg");
+	}
+}
+function initTheme(){ 
+    theme = Cookies.get('theme');
+    if(theme =='1'){
+        themeDark();
+    }
+    if(theme == '0'){
+        themeLight();
+    }
+initThemeSwitch();
+} 
+initTheme();
+
+$(document).ready(function(){
+  theme = Cookies.get('theme');
+  $("#theme-switch").click(function() {
+    if(theme =='1' || theme == '3'){
+        Cookies.set('theme',0);
+        themeLight();
+        $("#theme-switch-icon").attr("src", "static/icon/icon-light.svg");
+    }if(theme == "0" || theme == '2'){
+      Cookies.set('theme', '1', { expires: 3650});
+      themeDark();
+      $("#theme-switch-icon").attr("src", "static/icon/icon-dark.svg");
+    }
+    initThemeSwitch();
+  });
+});
